5e0f2883f5429c2c35f4d1469ebd45534e99e26f
Antoine Wencel <antoine.wencel@gmail.com>
2023-01-06 15:28:23 +0100

    new build

--- TicTacToe_2016.py
+++ TicTacToe_2016.py
@@ -9,8 +9,9 @@ def intro():
           "marking the spaces in a 3*3 grid. The player who succeeds in placing "
           "three of their marks in a horizontal, vertical, or diagonal row wins.")
     print("\n")
-    input("Press enter to continue.")
+    input("Press enter to continue!")
     print("\n")
+
 
 
 
